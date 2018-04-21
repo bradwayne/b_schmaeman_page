b4edea99894bafa02ccc9792454c22001e68f394
unknown <bradwayne74@gmail.com>
2018-04-21 13:05:21 -0400

    formspree email added

---
+++ assets/js/email.js
@@ -0,0 +1,21 @@
+$('#contact-form').submit(function (e) {
+    var name    = document.getElementById('inputName'),
+        email   = document.getElementById('inputEmail'),
+        subject = document.getElementById('inputSubject'),
+        message = document.getElementById('inputMessage');
+
+    if (!name.value || !email.value || !subject.value || !message.value) {
+        // alertify.error('Please check your entries')
+    } else {
+        $.ajax({
+            url: "https://formspree.io/me@bradley-schmaeman.com",
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json'
+        });
+
+        e.preventDefault()
+        $(this).get(0).reset()
+        // alertify.success('Message Sent')
+    }
+})
